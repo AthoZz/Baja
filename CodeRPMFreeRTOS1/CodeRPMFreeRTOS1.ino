ac57973176a2160ebf19fb9cb367825167554a3c
Victor888 <hutv2101@usherbrooke.ca>
2024-04-02 17:27:48 -0400

    Version finale enregistrement excel
    Lorsque l'arduino est déconnecté, un fichier excel se créer automatiquement avec les valeurs de RPM (dédoublé) a chaque seconde.
    On ne peut pas utiliser le serialMonitor de arduino IDE en même temps que le programme python.

--- CodeRPMFreeRTOS1/CodeRPMFreeRTOS1.ino
+++ CodeRPMFreeRTOS1/CodeRPMFreeRTOS1.ino
@@ -103,23 +103,12 @@ void PrintRPM( void * pvParameters )
       }
       averageRPM2 = averageRPM2/SIZE_BUFFER;
 
-      Serial.println("RPM du tachymètre 1 : " + String(averageRPM1));
-      //byte * RPM_Send = (byte *) &averageRPM1;  // À voir si on veut envoyer des floats ou des int c'est suffisant
-      //Serial.write(short(averageRPM1));       // A décommenter si on veut juste des valeurs entières
-            // À voir si il faut pas envoyer un string avec un \n a la place
-      //Serial.write(RPM_Send, sizeof(averageRPM1));
-
-
-      Serial.println("RPM du tachymètre 2 : " + String(averageRPM2));
-      //byte * RPM2_Send = (byte * ) &averageRPM2;
-      //Serial.write(short(averageRPM2));
-      // À voir si il faut pas envoyer un string avec un \n a la place
-      //Serial.write(RPM2_Send, sizeof(averageRPM2));  // A décommenter si on veut juste des valeurs entières
-      
-
-      Serial.println(digitalRead(7));
-      Serial.println(digitalRead(8));
-
+      //Serial.println("RPM du tachymètre 1 : " + String(averageRPM1));
+      //Serial.println("RPM du tachymètre 2 : " + String(averageRPM2));
+      //Serial.println(digitalRead(7));
+      //Serial.println(digitalRead(8));
+      Serial.println(String(averageRPM1));
+      Serial.println(String(averageRPM2));
       vTaskDelayUntil( &lastPrint, 1000/portTICK_PERIOD_MS );
      }
  }
